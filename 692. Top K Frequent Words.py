b7bb0b178ea86b8d8a7dc1ffd06daa14c1358c46
Deepanshu mehta <40016257+deepanshumehtaa@users.noreply.github.com>
2022-09-23 22:35:08 +0530

    Update 692. Top K Frequent Words.py

--- 692. Top K Frequent Words.py
+++ 692. Top K Frequent Words.py
@@ -1,4 +1,8 @@
 """
+Given an array of strings words and an integer k, return the k most frequent strings.
+
+Return the answer sorted by the frequency from highest to lowest. Sort the words with the same frequency by their lexicographical order.
+
 ["i","love","leetcode","i","love","coding"]
 2
 """
